07652391817f0df00d88b3bbaad997108d860ad8
neil_mishh <thensm9neil@gmail.com>
2025-03-23 15:19:57 +1300

    Set up endpoints to Get User info asynchronously

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47dcaeb3b4a454f7409a9212497b8c9a56e1fb7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f338c6c2366110f421d768cf79f6a9fe048582c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
