353130586bcc3243146d791307b50831779c38b4
neil_mishh <thensm9neil@gmail.com>
2025-03-30 00:08:40 +1300

    pulling values from parent component to child

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  usersFromHomeComponent = input.required<any>()
+  model: any = {}
 
+  register (){
+    console.log(this.model);
+  }
+
+  cancel(){
+    console.log("Cancelled");
+  }
 }
