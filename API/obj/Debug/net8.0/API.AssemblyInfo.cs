32812b4f7a02f9fef61176eba0a9f12769951427
neil_mishh <thensm9neil@gmail.com>
2025-03-31 22:32:48 +1300

    create repository for db context and add it to services

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6054b56241e9a2a708df43015b10bda09c5ae58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca181f230affc8be37175c1c7568164aed7cd19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
