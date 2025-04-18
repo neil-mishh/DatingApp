81024af386a83b965a197a716ef3468f5e279e81
neil_mishh <thensm9neil@gmail.com>
2025-04-18 15:27:25 +1200

    Persist changes when leaving site

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+703b7f7062a3724fe3868f6d701b8e7102697cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2329bcbbf5723fca6e24a7a46864477fd9fe28")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
